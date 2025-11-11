53bf492a9fbdf0dfdc265069dc0808925d0d35f6
liamCtrl <122392454+liamCtrl@users.noreply.github.com>
2025-11-11 15:36:06 +0000

    Tests reach 100% coverage

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -1,4 +1,5 @@
 namespace AtmServices.Test;
+
 using AtmServices;
 public class AtmTests
 
@@ -14,19 +15,43 @@ public class AtmTests
 
     }
 
- 
 
-        [Fact]
+
+    [Fact]
 
     public void Test_Withdraw()
 
     {
-
         var result = testAtm.withdraw(25);
 
         Assert.True(result);
 
-        Assert.Equal(65, testAtm.getBalance());
+        Assert.Equal(75, testAtm.getBalance());
+        
+        var result2 = testAtm.withdraw(200);
+
+        Assert.False(result2);
+
+        Assert.Equal(75, testAtm.getBalance());
+        
+    }
+    
+
+    [Fact]
+
+    public void Test_Deposit()
+    {
+        var result = testAtm.deposit(25);
+
+        Assert.True(result);
+
+        Assert.Equal(125, testAtm.getBalance());
+
+        var result2 = testAtm.deposit(-50);
+
+        Assert.False(result2);
+
+        Assert.Equal(125, testAtm.getBalance());
 
     }
 
